3bb2824417113ccbd6ef1887ace45d66d8b270e8
Simon Castle <simon.castle@metaswitch.com>
2020-12-02 15:38:43 +0000

    Solved day 2, very inelegant

--- 2020/all_days/src/bin/day02.rs
+++ 2020/all_days/src/bin/day02.rs
@@ -1,23 +1,54 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("src/bin/input/input02")
+    let input: Vec<String> = fs::read_to_string("src/bin/input/input02")
         .expect("Failed to read file")
         .lines()
         .map(std::string::ToString::to_string)
         .collect();
     println!("Day 2 Part 1: {}", count_valid_passwords(&input));
-    println!("Day 2 Part 2: {}", "???");
-
+    println!("Day 2 Part 2: {}", count_actual_valid_passwords(&input));
 }
 
-fn count_valid_passwords(input: &Vec<String>) -> usize {
-    input.clone().into_iter().filter(|password| validate_password(password.to_string())).count()
+fn count_valid_passwords(input: &[String]) -> usize {
+    input.into_iter().filter(|line| validate_password(line.to_string())).count()
 }
 
-fn validate_password(_password: String) -> bool {
+fn validate_password(line: String) -> bool {
     // Parse the line.  Example: 4-7 z: zzzfzlzzz
     // <lower>-<upper> <letter>: <passwordString>
+    // Then: (a) search for <letter> in <passwordString> and (b) confirm the number of occurences is within [lower, upper]
 
-    true
+    let line_parts: Vec<&str> = line.split(|c| c == ' ' || c == '-').collect();
+    assert!(line_parts.len() == 4);
+    let lower_bound = line_parts[0].parse().expect("Lower bound not a number");
+    let upper_bound = line_parts[1].parse().expect("Upper bound not a number");
+    let required_letter = line_parts[2].chars().nth(0).expect("Mis-parsed line");
+    let password = line_parts[3];
+    
+    let letter_count = password.len() - password.replace(required_letter, "").len();
+
+    letter_count >= lower_bound && letter_count <= upper_bound
+}
+
+fn count_actual_valid_passwords(input: &[String]) -> usize {
+    input.into_iter().filter(|line| validate_real_password(line.to_string())).count()
+}
+
+fn validate_real_password(line: String) -> bool {
+    // Parse the line.  Example: 4-7 z: zzzfzlzzz
+    // <lower>-<upper> <letter>: <passwordString>
+    // Then: (a) search for <letter> in <passwordString> and (b) confirm the number of occurences is within [lower, upper]
+
+    let line_parts: Vec<&str> = line.split(|c| c == ' ' || c == '-').collect();
+    assert!(line_parts.len() == 4);
+    let lower_bound: usize = line_parts[0].parse().expect("Lower bound not a number");
+    let upper_bound: usize = line_parts[1].parse().expect("Upper bound not a number");
+    let required_letter = line_parts[2].chars().nth(0).expect("Mis-parsed line");
+    let password = line_parts[3];
+
+    let first_is_char = password.chars().nth(lower_bound-1).expect("Password not long enough for lower bound") == required_letter;
+    let second_is_char = password.chars().nth(upper_bound-1).expect("Password not long enough for upper bound") == required_letter;
+
+    (first_is_char || second_is_char) && !(first_is_char && second_is_char)
 }
