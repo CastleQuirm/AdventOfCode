466572c344a285c42da730f423183753d5b092ec
Simon Castle <simon.castle@metaswitch.com>
2020-12-02 18:38:35 +0000

    Add empty input files

--- 2020/all_days/src/create_script.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-f = open("day" + sys.argv[1] + ".rs", "w")
-f.write("pub fn day" + sys.argv[1] + "(input_lines: &[String]) -> (u64, u64) {\n")
-f.write("    (0, 0)\n")
-f.write("}\n")
-f.close()
