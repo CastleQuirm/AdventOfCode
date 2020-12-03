77b62e77e4dcf7a2d32806b8c41dcc4cdc1966f9
Simon Castle <simon.castle@metaswitch.com>
2020-12-03 11:23:10 +0000

    Day 3 tweak - better Rust, worse perf

--- 2020/all_days/src/day3.rs
+++ 2020/all_days/src/day3.rs
@@ -1,11 +1,13 @@
 pub fn day3(input_lines: &[String]) -> (u64, u64) {
-    let run1 = check_slope(input_lines, 1, 1);
-    let run2 = check_slope(input_lines, 1, 3);
-    let run3 = check_slope(input_lines, 1, 5);
-    let run4 = check_slope(input_lines, 1, 7);
-    let run5 = check_slope(input_lines, 2, 1);
+    let slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)];
 
-    (run2, run1 * run2 * run3 * run4 * run5)
+    (
+        check_slope(input_lines, 1, 3),
+        slopes
+            .iter()
+            .map(|&(x, y)| check_slope(input_lines, x, y))
+            .product(),
+    )
 }
 
 fn check_slope(input_lines: &[String], x: usize, y: usize) -> u64 {
