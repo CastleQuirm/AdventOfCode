fd7076a009d0bcc6384d2a0adfacfa4b42ecc46a
Simon Castle <simon.castle@metaswitch.com>
2020-12-03 10:45:27 +0000

    Cargo fmt

--- 2020/all_days/src/day3.rs
+++ 2020/all_days/src/day3.rs
@@ -9,5 +9,14 @@ pub fn day3(input_lines: &[String]) -> (u64, u64) {
 }
 
 fn check_slope(input_lines: &[String], x: usize, y: usize) -> u64 {
-    (0..input_lines.len()).filter(|&row| row % x == 0).filter(|&row| input_lines[row].chars().nth((y*row/x) % input_lines[row].len()).expect("Couldn't find character") == '#').count() as u64
+    (0..input_lines.len())
+        .filter(|&row| row % x == 0)
+        .filter(|&row| {
+            input_lines[row]
+                .chars()
+                .nth((y * row / x) % input_lines[row].len())
+                .expect("Couldn't find character")
+                == '#'
+        })
+        .count() as u64
 }
