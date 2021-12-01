ffec848bd3c1f9581e1e8c8285884b1e53767eae
Simon Castle <simon.castle@metaswitch.com>
2021-12-01 11:05:39 +0000

    Add template for future years

---
+++ template/src/day08.rs
@@ -0,0 +1,6 @@
+// Potential improvements:
+//
+
+pub fn day08(_input_lines: &[String]) -> (u64, u64) {
+    (0, 0)
+}
---
+++ template/src/day22.rs
@@ -0,0 +1,6 @@
+// Potential improvements:
+//
+
+pub fn day22(_input_lines: &[String]) -> (u64, u64) {
+    (0, 0)
+}
