a115c165457afd9a26a4c64f800f7df351ffaeb2
Simon Castle <simon.castle@metaswitch.com>
2021-12-02 08:02:04 +0000

    Day 02 solution

--- year21/src/day02.rs
+++ year21/src/day02.rs
@@ -1,6 +1,35 @@
 // Potential improvements:
 //
 
-pub fn day02(_input_lines: &[String]) -> (u64, u64) {
-    (0, 0)
+pub fn day02(input_lines: &[String]) -> (u64, u64) {
+    let (part1_distance, part1_depth) = input_lines.iter().fold((0,0), |acc, instruction| {
+        let (direction, val) = direction_and_val(instruction);
+        match direction {
+            "forward" => (acc.0 + val, acc.1),
+            "up" => (acc.0, acc.1 - val),
+            "down" => (acc.0, acc.1 + val),
+            _ => panic!("Unrecognised direction"),
+        }
+    });
+    
+    let (part2_distance, part2_depth, _) = input_lines.iter().fold((0,0,0), |acc, instruction| {
+        let (direction, val) = direction_and_val(instruction);
+        match direction {
+            "forward" => (acc.0 + val, acc.1 + val * acc.2, acc.2),
+            "up" => (acc.0, acc.1, acc.2 - val),
+            "down" => (acc.0, acc.1, acc.2 + val),
+            _ => panic!("Unrecognised direction"),
+        }
+    });
+
+    
+    
+    ((part1_distance * part1_depth), (part2_distance * part2_depth))
+}
+
+fn direction_and_val(line: &String) -> (&str, u64) {
+    let splitline = line.split(" ").collect::<Vec<&str>>();
+    let direction = splitline.first().expect("No first part of splitline");
+    let val = splitline.last().expect("No second part of splitline").to_string().parse::<u64>().expect("Couldn't parse the second part as a u64");
+    (direction, val)
 }
