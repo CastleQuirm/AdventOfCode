6052d03724e8ac100a8cea7d37b2d9bb101b4f95
Simon Castle <simon.c.castle@gmail.com>
2021-12-17 08:33:05 +0000

    Day 17 complete

--- year21/src/day17.rs
+++ year21/src/day17.rs
@@ -1,54 +1,54 @@
 // Potential improvements:
-//
+// I should probably rework the two parts so that we get a single vec of all the trajectories
+// then part 1 is "look for the max y and triangle number" and part 2 is "just count the list".
 
 pub fn day17(input_lines: &[String]) -> (u64, u64) {
-    let target = Target::new(input_lines[0]);
+    let target = Target::new(input_lines);
 
-
-    // // Let's produce a hashmap of valid starting Xs to the number of steps when they'll land in range (noting we'll need something special for "infinitely far")
-    // let mut steps_for_starting_x: HashMap<u64, (u64, Option<u64>)> = HashMap::new();
-    // let mut x = 0;
-    // while x < max_x {
-    //     x += 1;
-    //     let triangle_x = x * (x+1) / 2;
-    //     // Check if triangle numbers let us ever reach it?
-    //     if triangle_x < min_x { continue; }
-    //     // Fewest steps to reach the range...
-    //     while steps 
-    //     let fewest_steps = ???;
-    //     let most_steps = if triangle_x <= max_x {
-    //         None
-    //     } else {
-    //         if ??? < fewest_steps { continue; }
-    //         Some(???)
-    //     }
-    //     if most_steps < 
-    //     steps_for_starting_x.insert(x, (fewest_steps, most_steps));
-    // }
-
-    let best_y = (0..target.max_x+1).filter_map(|starting_x| {
-        // Bypass x that's too short
-        if starting_x * (starting_x+1) / 2 < target.min_x { return None; }
-        (target.min_y..0).find(|&starting_y| {
-            // Return true if we hit the target, false if we don't 
-            target.does_trajectory_hit(starting_x, starting_y.abs())
+    let best_y = (0..target.max_x + 1)
+        .filter_map(|starting_x| {
+            // Bypass x that's too short
+            if starting_x * (starting_x + 1) / 2 < target.min_x {
+                return None;
+            }
+            (target.min_y..0).find(|&starting_y| {
+                // Return true if we hit the target, false if we don't
+                target.does_trajectory_hit(starting_x, starting_y.abs())
+            })
         })
-    }).max().expect("No best y");
+        .min()
+        .expect("No best y")
+        .abs();
+
+    let part2: u64 = (0..target.max_x + 1)
+        .map(|starting_x| {
+            // Bypass x that's too short
+            if starting_x * (starting_x + 1) / 2 < target.min_x {
+                return 0;
+            }
+            (target.min_y..target.min_y.abs())
+                .filter(|&starting_y| {
+                    // Return true if we hit the target, false if we don't
+                    target.does_trajectory_hit(starting_x, starting_y)
+                })
+                .count()
+        })
+        .sum::<usize>() as u64;
 
     let part1 = (best_y * (best_y + 1) / 2) as u64;
 
-    (part1, 0)
+    (part1, part2)
 }
 
 struct Target {
     min_x: i64,
     max_x: i64,
     min_y: i64,
-    max_y: i64
+    max_y: i64,
 }
 impl Target {
-    fn new(input: String) -> Self {
-        let coordinate_ranges = input
+    fn new(input: &[String]) -> Self {
+        let coordinate_ranges = input[0]
             .split(": ")
             .nth(1)
             .expect("")
@@ -80,7 +80,12 @@ impl Target {
         let min_y = y_values[0];
         let max_y = y_values[1];
 
-        Self { min_x, max_x, min_y, max_y }
+        Self {
+            min_x,
+            max_x,
+            min_y,
+            max_y,
+        }
     }
 
     fn does_trajectory_hit(&self, starting_x: i64, starting_y: i64) -> bool {
@@ -90,17 +95,20 @@ impl Target {
         let mut velocity_y = starting_y;
 
         while current_x <= self.max_x && current_y >= self.min_y {
-            if current_x >= self.min_x && current_y <= self.max_y { return true; }
+            if current_x >= self.min_x && current_y <= self.max_y {
+                return true;
+            }
             current_x += velocity_x;
             current_y += velocity_y;
-            if velocity_x > 0 { velocity_x -= 1; }
+            if velocity_x > 0 {
+                velocity_x -= 1;
+            }
             velocity_y -= 1;
         }
-    
+
         false
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -112,6 +120,6 @@ mod tests {
             .lines()
             .map(std::string::ToString::to_string)
             .collect::<Vec<String>>();
-        assert_eq!(day17(&input_lines), (45, 0));
+        assert_eq!(day17(&input_lines), (45, 112));
     }
 }
