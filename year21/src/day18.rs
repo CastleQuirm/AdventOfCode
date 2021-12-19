cc3daf7b43494f8f3ed64c042c7e60e558b1803c
Simon Castle <simon.c.castle@gmail.com>
2021-12-19 08:14:04 +0000

    Add day 18 full UT and fix silly bug it spotted!

--- year21/src/day18.rs
+++ year21/src/day18.rs
@@ -24,7 +24,7 @@ pub fn day18(input_lines: &[String]) -> (u64, u64) {
         .magnitude();
 
     let part2 = snumbers
-        .combinations(2)
+        .permutations(2)
         .map(|pair| pair[0].add(&pair[1]).magnitude())
         .max()
         .expect("No max?");
@@ -371,11 +371,20 @@ mod tests {
 
     #[test]
     fn check_day18() {
-        let input_lines = ""
+        let input_lines = "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
             .lines()
             .map(std::string::ToString::to_string)
             .collect::<Vec<String>>();
-        assert_eq!(day18(&input_lines), (0, 0));
+        assert_eq!(day18(&input_lines), (4140, 3993));
     }
 
     #[test]
