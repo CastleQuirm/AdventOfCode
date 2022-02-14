7608b584cb5c22d3319368f69ca328724b91f9a2
Simon Castle <simon.castle@metaswitch.com>
2022-02-14 18:14:31 +0000

    2018 Day 02 complete

--- year18/src/day02.rs
+++ year18/src/day02.rs
@@ -2,8 +2,13 @@
 //
 
 use std::collections::{HashMap, HashSet};
+use itertools::Itertools;
 
 pub fn day02(input_lines: &[Vec<String>]) -> (String, String) {
+    (day02_part1(input_lines), day02_part2(input_lines))
+}
+
+fn day02_part1(input_lines: &[Vec<String>]) -> String {
     let doubles_and_triples = input_lines[0].iter().fold((0, 0), |counts, line| {
         let duplicate_letters = count_duplicates(line);
         (
@@ -12,9 +17,16 @@ pub fn day02(input_lines: &[Vec<String>]) -> (String, String) {
         )
     });
 
-    let answer1 = doubles_and_triples.0 * doubles_and_triples.1;
-    let answer2 = 0;
-    (format!("{}", answer1), format!("{}", answer2))
+    format!("{}", doubles_and_triples.0 * doubles_and_triples.1)
+}
+
+fn day02_part2(input_lines: &[Vec<String>]) -> String {
+    let word_len = input_lines[0][0].len();
+    let answer2 = input_lines[0].iter().combinations(2).find_map(|pair| {
+        let intersection = string_intersection(pair[0], pair[1]);
+        if intersection.len() == word_len - 1 { Some(intersection) } else { None }
+    });
+    answer2.expect("Couldn't find an answer")
 }
 
 fn count_duplicates(line: &str) -> (u64, u64) {
@@ -29,31 +41,45 @@ fn count_duplicates(line: &str) -> (u64, u64) {
     (contains_double, contains_triple)
 }
 
+// Considered doing the specific thing for the puzzle (return Option<String> with the value if it's the one we want)
+// but decided to instead have a nicer function and have the caller do the smarts.
+fn string_intersection(string_a: &str, string_b: &str) -> String {
+    let mut intersection = "".to_string();
+    let chars_a = string_a.chars();
+    let mut chars_b = string_b.chars();
+    for char_1 in chars_a {
+        let char_2 = chars_b.next().expect("Word 2 was shorter than word 1");
+        if char_1 == char_2 { intersection.push(char_1) }
+    }
+    assert_eq!(chars_b.next(), None);
+    intersection
+}
+
 #[cfg(test)]
 mod tests {
-    use super::day02;
+    use super::day02_part1;
+    use super::day02_part2;
     use crate::utils::load_input;
 
     #[test]
     fn check_day02_case01() {
-        full_test(
-            "abcdef
+        assert_eq!(day02_part1(&load_input("abcdef
 bababc
 abbcde
 abcccd
 aabcdd
 abcdee
-ababab", // INPUT STRING
-            "12", // PART 1 RESULT
-            "0",  // PART 2 RESULT
-        )
+ababab")), "12");
     }
 
-    fn full_test(input_text: &str, part1_result: &str, part2_result: &str) {
-        let input_lines = load_input(input_text);
-        assert_eq!(
-            day02(&input_lines),
-            (part1_result.to_string(), part2_result.to_string())
-        );
+    #[test]
+    fn check_day02_case02() {
+        assert_eq!(day02_part2(&load_input("abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz")), "fgij");
     }
 }
