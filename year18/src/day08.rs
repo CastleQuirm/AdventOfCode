8001f3ead4cc1031031cc8bfb8697e6fd92a7833
Simon Castle <simon.c.castle@gmail.com>
2022-05-21 11:58:48 +0100

    Year 18 Day 08 - Complete

--- year18/src/day08.rs
+++ year18/src/day08.rs
@@ -1,10 +1,68 @@
 // Potential improvements:
-//
+// 1. Could/should cache node's values for efficiency.
 
-pub fn day08(_input_lines: &[Vec<String>]) -> (String, String) {
-    let answer1 = 0;
-    let answer2 = 0;
+pub fn day08(input_lines: &[Vec<String>]) -> (String, String) {
+    // Read the input
+    let digits = input_lines[0][0]
+        .split(' ')
+        .into_iter()
+        .map(|s| s.parse::<usize>().expect("Didn't parse a number"))
+        .collect::<Vec<usize>>();
+    let (root, parsed_digits) = Node::parse(&digits);
+    assert_eq!(parsed_digits, digits.len());
+
+    // Get the answers
+    let answer1 = root.metadata_sum();
+    let answer2 = root.value();
     (format!("{}", answer1), format!("{}", answer2))
+}
+
+struct Node {
+    children: Vec<Node>,
+    metadata: Vec<usize>,
+}
+
+impl Node {
+    fn parse(digits: &[usize]) -> (Self, usize) {
+        assert!(digits.len() >= 3);
+
+        let number_of_children = digits[0];
+        let number_of_metadata = digits[1];
+        let mut consumed_digits = 2;
+
+        let mut children: Vec<Node> = Vec::new();
+        for _ in 0..number_of_children {
+            let (child, movement) = Node::parse(&digits[consumed_digits..]);
+            consumed_digits += movement;
+            children.push(child);
+        }
+
+        let metadata: Vec<usize> =
+            digits[consumed_digits..(consumed_digits + number_of_metadata)].to_vec();
+        consumed_digits += number_of_metadata;
+
+        (Node { children, metadata }, consumed_digits)
+    }
+
+    fn metadata_sum(&self) -> usize {
+        self.children
+            .iter()
+            .fold(self.metadata.iter().sum(), |acc, child| {
+                acc + child.metadata_sum()
+            })
+    }
+
+    fn value(&self) -> usize {
+        if self.children.is_empty() {
+            self.metadata_sum()
+        } else {
+            self.metadata
+                .iter()
+                .filter_map(|child_ix| self.children.get(*child_ix - 1))
+                .map(|child| child.value())
+                .sum()
+        }
+    }
 }
 
 #[cfg(test)]
@@ -15,9 +73,9 @@ mod tests {
     #[test]
     fn check_day08_case01() {
         full_test(
-            "",  // INPUT STRING
-            "0", // PART 1 RESULT
-            "0", // PART 2 RESULT
+            "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2", // INPUT STRING
+            "138",                                 // PART 1 RESULT
+            "66",                                  // PART 2 RESULT
         )
     }
 
