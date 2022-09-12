0da68a265163b764f49dd0b5cc4c91b8ad53d714
Simon Castle <simon.castle@metaswitch.com>
2022-09-12 09:40:45 +0100

    Year 18 Day 14

--- year18/src/day14.rs
+++ year18/src/day14.rs
@@ -1,10 +1,63 @@
 // Potential improvements:
 //
 
-pub fn day14(_input_lines: &[Vec<String>]) -> (String, String) {
-    let answer1 = 0;
-    let answer2 = 0;
-    (format!("{}", answer1), format!("{}", answer2))
+pub fn day14(input_lines: &[Vec<String>]) -> (String, String) {
+    let num_recipies = input_lines[0][0].parse::<usize>().expect("");
+    // This next line breaks if the input starts with a 0 - which doesn't make much sense for the whole puzzle,
+    // but does break one of the test examples in part 2.
+    let answer2_target = num_recipies.to_string();
+    let target_len = num_recipies.to_string().len();
+    let mut recipies = vec![3, 7];
+    let mut elf_positions = vec![0, 1];
+    let mut answer2 = None;
+
+    while (recipies.len() < num_recipies + 10) || answer2.is_none() {
+        create_recipies(&mut recipies, &mut elf_positions);
+        if answer2.is_none() && recipies.len() > target_len {
+            let potential_answer2 = recipies.len() - target_len - 1;
+            if digits_as_str(&recipies[potential_answer2..potential_answer2 + target_len])
+                == answer2_target
+            {
+                answer2 = Some(potential_answer2);
+            }
+            let potential_answer2 = potential_answer2 + 1;
+            if digits_as_str(&recipies[potential_answer2..potential_answer2 + target_len])
+                == answer2_target
+            {
+                answer2 = Some(potential_answer2);
+            }
+        }
+    }
+
+    let answer1 = digits_as_str(&recipies[num_recipies..(num_recipies + 10)]);
+    (format!("{}", answer1), format!("{}", answer2.unwrap()))
+}
+
+fn create_recipies(recipies: &mut Vec<usize>, elf_positions: &mut Vec<usize>) {
+    let new_sum = elf_positions
+        .iter()
+        .map(|&elf| recipies[elf])
+        .sum::<usize>();
+    assert!(new_sum < 20);
+    if new_sum >= 10 {
+        recipies.push(1);
+        recipies.push(new_sum - 10);
+    } else {
+        recipies.push(new_sum);
+    }
+
+    *elf_positions = elf_positions
+        .iter()
+        .map(|&elf| (elf + recipies[elf] + 1) % recipies.len())
+        .collect::<Vec<usize>>();
+}
+
+fn digits_as_str(recipies: &[usize]) -> String {
+    recipies
+        .iter()
+        .map(|&i| i.to_string())
+        .collect::<Vec<String>>()
+        .concat()
 }
 
 #[cfg(test)]
@@ -15,9 +68,18 @@ mod tests {
     #[test]
     fn check_day14_case01() {
         full_test(
-            "",  // INPUT STRING
-            "0", // PART 1 RESULT
-            "0", // PART 2 RESULT
+            "9",          // INPUT STRING
+            "5158916779", // PART 1 RESULT
+            "0",          // PART 2 RESULT
+        )
+    }
+
+    #[test]
+    fn check_day14_case02() {
+        full_test(
+            "59414", // INPUT STRING
+            "0",     // PART 1 RESULT
+            "5",     // PART 2 RESULT
         )
     }
 
