fc2fbbb5a7403278ea8ceeedf3df9b633ff8826c
Simon Castle <simon.c.castle@gmail.com>
2022-10-02 09:07:28 +0100

    Year 20 Day 1 itertools solution

--- year20/src/day1.rs
+++ year20/src/day1.rs
@@ -1,35 +1,54 @@
 // Potential improvements:
 // 1: Make the format more of a functional programming style
 // 2: Improve on the O(n^2) and O(n^3) behaviours.
+use itertools::Itertools;
 
 pub fn day1(input_lines: &[String]) -> (u64, u64) {
-    let input_numbers: Vec<u64> = input_lines
+    (
+        find_product_with_sum_2020(input_lines, 2),
+        find_product_with_sum_2020(input_lines, 3),
+    )
+}
+
+fn find_product_with_sum_2020(input_lines: &[String], element_count: usize) -> u64 {
+    input_lines
         .iter()
         .map(|line| line.parse::<u64>().expect("Line wasn't a number"))
-        .collect();
-    (calculate2(&input_numbers), calculate3(&input_numbers))
+        .combinations(element_count)
+        .find(|c| c.iter().copied().sum::<u64>() == 2020)
+        .expect("No solution?")
+        .into_iter()
+        .product()
 }
 
-fn calculate2(input: &[u64]) -> u64 {
-    for i in 0..input.len() {
-        for j in i + 1..input.len() {
-            if input[i] + input[j] == 2020 {
-                return input[i] * input[j];
-            }
-        }
-    }
-    0
-}
+// pub fn day1(input_lines: &[String]) -> (u64, u64) {
+//     let input_numbers: Vec<u64> = input_lines
+//         .iter()
+//         .map(|line| line.parse::<u64>().expect("Line wasn't a number"))
+//         .collect();
+//     (calculate2(&input_numbers), calculate3(&input_numbers))
+// }
 
-fn calculate3(input: &[u64]) -> u64 {
-    for i in 0..input.len() {
-        for j in i + 1..input.len() {
-            for k in j + 1..input.len() {
-                if input[i] + input[j] + input[k] == 2020 {
-                    return input[i] * input[j] * input[k];
-                }
-            }
-        }
-    }
-    0
-}
+// fn calculate2(input: &[u64]) -> u64 {
+//     for i in 0..input.len() {
+//         for j in i + 1..input.len() {
+//             if input[i] + input[j] == 2020 {
+//                 return input[i] * input[j];
+//             }
+//         }
+//     }
+//     0
+// }
+
+// fn calculate3(input: &[u64]) -> u64 {
+//     for i in 0..input.len() {
+//         for j in i + 1..input.len() {
+//             for k in j + 1..input.len() {
+//                 if input[i] + input[j] + input[k] == 2020 {
+//                     return input[i] * input[j] * input[k];
+//                 }
+//             }
+//         }
+//     }
+//     0
+// }
