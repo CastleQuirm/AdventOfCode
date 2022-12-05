bc2ae0d3b6af3cbae70fd850ab3fd1ddd3306cab
Simon Castle <simon.c.castle@gmail.com>
2022-12-05 08:17:09 +0000

    Year 22 Day 05 starting point

--- year22/src/day05.rs
+++ year22/src/day05.rs
@@ -1,6 +1,24 @@
+use regex::Regex;
+
 pub fn day05(input_lines: &str) -> (String, String) {
-    let _ = input_lines;
-    let answer1 = 0;
+    // Read the first part of the input into a Vec of mutable Vecs
+
+    let mut crates: Vec<Vec<char>> = vec![[vec![]; 9]];
+
+    // Process the instructions
+    let re = Regex::new(r"move (\d+) from (\d) to (\d)").unwrap();
+    for line in instructions {
+        let instruction = re.captures(line).expect("Couldn't regex");
+        let move_count = instruction.get(1).expect("Not found").parse::<usize>().expect("Couldn't parse");
+        let from_stack = instruction.get(2).expect("Not found").parse::<usize>().expect("Couldn't parse");
+        let to_stack = instruction.get(3).expect("Not found").parse::<usize>().expect("Couldn't parse");
+
+        for _ in 0..move_count {
+            crates[to_stack].push(crates[from_stack].pop());
+        }
+    }
+
+    let answer1 = crates.iter().map(|stack| stack.pop().unwrap_or(' ')).collect::<String>();
     let answer2 = 0;
     (format!("{}", answer1), format!("{}", answer2))
 }
@@ -21,6 +39,14 @@ mod tests {
 
     #[test]
     fn check_day05_both_case1() {
-        assert_eq!(day05(""), ("0".to_string(), "0".to_string()))
+        assert_eq!(day05("    [D]    
+[N] [C]    
+[Z] [M] [P]
+    1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"), ("CMZ".to_string(), "0".to_string()))
     }
 }
