5ffd224008e24a4a5eb97fd068e974c68488d165
Simon Castle <simon.c.castle@gmail.com>
2022-12-09 11:20:16 +0000

    Year 22 Day 09 Completed

--- year22/src/day09.rs
+++ year22/src/day09.rs
@@ -8,51 +8,64 @@ pub fn day09(input_lines: &str) -> (String, String) {
         .map(|line| line.parse::<HeadMove>().unwrap())
         .collect::<Vec<_>>();
 
-    (format!("{}", move_rope(&head_moves, 1)), format!("{}", move_rope(&head_moves, 10)))
+    (format!("{}", move_rope(&head_moves, 1)), format!("{}", move_rope(&head_moves, 9)))
 }
 
 fn move_rope(head_moves: &[HeadMove], rope_len: usize) -> usize {
     let mut tail_visited = HashSet::from([Coord2::new(0, 0)]);
     let mut head_location = Coord2::new(0, 0);
-    let mut tail_location = Coord2::new(0, 0);
+    let mut knot_locations = vec![Coord2::new(0, 0); rope_len];
 
     head_moves.iter().for_each(|instruction| {
+        // println!("Apply move: {:?}", instruction);
         let head_delta = Coord2::movement(&instruction.direction);
         (0..instruction.distance).for_each(|_| {
+            // println!("- Move!");
             // Move the head
             head_location.moved(&head_delta);
             // Catch up the tail if necessary
-            // Check some edge cases
-            match head_location.manhattan_dist(&tail_location) {
-                0 | 1 => (), // no-op if the head is still adjacent or over the tail
-                2 => {
-                    // 2 distance could be diagonally adjacent, no-op, or straight line, tail moves straight
-                    if head_location.x == tail_location.x || head_location.y == tail_location.y {
-                        // Straight line, we have to move straight.  It must be in the same direction as the head moved.
-                        tail_location.moved(&head_delta);
+            let mut prev_knot_location = head_location;
+            knot_locations.iter_mut().for_each(|knot| {
+                // println!("Prev knot {:?}; next knot {:?}", prev_knot_location, knot);
+                match prev_knot_location.manhattan_dist(&knot) {
+                    0 | 1 => (), // no-op if the previous knot is still adjacent or over the next knot
+                    2 => {
+                        // 2 distance could be diagonally adjacent, no-op, or straight line, tail moves straight
+                        if prev_knot_location.x == knot.x || prev_knot_location.y == knot.y {
+                            // Straight line, we have to move straight.
+                            let knot_delta = Coord2::new(
+                                (prev_knot_location.x - knot.x) / 2,
+                                (prev_knot_location.y - knot.y) / 2,
+                            );
+                            knot.moved(&knot_delta); // TODO
+                        }
                     }
+                    3 | 4 => {
+                        // 3 distance means the tail must need to move diagonally (the head can't get three away in a straight line)
+                        assert_ne!(prev_knot_location.x, knot.x);
+                        assert_ne!(prev_knot_location.y, knot.y);
+                        let knot_delta = Coord2::new(
+                            (prev_knot_location.x - knot.x)
+                                / (prev_knot_location.x - knot.x).abs(),
+                            (prev_knot_location.y - knot.y)
+                                / (prev_knot_location.y - knot.y).abs(),
+                        );
+                        knot.moved(&knot_delta);
+                    }
+                    _ => panic!(), // Can't get 5 or more away
                 }
-                3 => {
-                    // 3 distance means the tail must need to move diagonally (the head can't get three away in a straight line)
-                    assert_ne!(head_location.x, tail_location.x);
-                    assert_ne!(head_location.y, tail_location.y);
-                    let tail_delta = Coord2::new(
-                        (head_location.x - tail_location.x)
-                            / (head_location.x - tail_location.x).abs(),
-                        (head_location.y - tail_location.y)
-                            / (head_location.y - tail_location.y).abs(),
-                    );
-                    tail_location.moved(&tail_delta);
-                }
-                _ => panic!(), // Can't get 4 or more away
-            }
+                // println!("New knot location {:?}", knot);
+                prev_knot_location = knot.clone();
+            });
+            
             // Make sure we've counted where the tail is.
-            tail_visited.insert(tail_location);
+            tail_visited.insert(knot_locations.last().unwrap().clone());
         })
     });
     tail_visited.len()
 }
 
+#[derive(Debug)]
 struct HeadMove {
     direction: Direction,
     distance: i64,
