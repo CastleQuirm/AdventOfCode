7e9e6660031f5684222c0b42d95491a2f9f00313
Simon Castle <simon.c.castle@gmail.com>
2022-12-12 13:02:33 +0000

    Year 22 Day 12 complete

--- year22/src/day12.rs
+++ year22/src/day12.rs
@@ -10,25 +10,15 @@ pub fn day12(input_lines: &str) -> (String, String) {
 
     let start_spot = find_char_coords(&raw_map, &'S');
     let end_spot = find_char_coords(&raw_map, &'E');
-    println!(
-        "Start at ({},{}), end at ({},{})",
-        start_spot.x, start_spot.y, end_spot.x, end_spot.y
-    );
 
-    let mut dijkstra_explored_map = HashMap::from([(
-        start_spot,
-        ExploredSpace {
-            distance: 0,
-            _direction_reached: None,
-        },
-    )]);
+    let mut dijkstra_explored_map = HashMap::from([(start_spot, 0)]);
 
     let mut newly_added = dijkstra_explored_map.clone();
 
     // while dijkstra_explored_map.len() < raw_map.len() * raw_map[0].len() {
     while !dijkstra_explored_map.contains_key(&end_spot) {
         let mut newly_newly_added = HashMap::new();
-        newly_added.iter().for_each(|(source, current_info)| {
+        newly_added.iter().for_each(|(source, current_distance)| {
             let current_height = raw_map[source.y as usize][source.x as usize];
             let current_height = if current_height == 'S' {
                 'a'
@@ -61,29 +51,8 @@ pub fn day12(input_lines: &str) -> (String, String) {
 
                     // Rely on ASCII ordering!
                     if candidate_height as usize <= current_height as usize + 1 {
-                        // Move here!
-                        println!(
-                            "Moving from ({},{}) to ({},{}), total distance {}",
-                            source.x,
-                            source.y,
-                            candidate_coords.x,
-                            candidate_coords.y,
-                            current_info.distance + 1
-                        );
-                        dijkstra_explored_map.insert(
-                            candidate_coords,
-                            ExploredSpace {
-                                distance: current_info.distance + 1,
-                                _direction_reached: Some(*direction),
-                            },
-                        );
-                        newly_newly_added.insert(
-                            candidate_coords,
-                            ExploredSpace {
-                                distance: current_info.distance + 1,
-                                _direction_reached: Some(*direction),
-                            },
-                        );
+                        dijkstra_explored_map.insert(candidate_coords, current_distance + 1);
+                        newly_newly_added.insert(candidate_coords, current_distance + 1);
                     }
                 }
             })
@@ -92,16 +61,65 @@ pub fn day12(input_lines: &str) -> (String, String) {
         // Replace the newly added list
         newly_added = newly_newly_added;
     }
-    // println!("OVERALL MAP ({} entries)", dijkstra_explored_map.len());
-    // for location in &dijkstra_explored_map {
-    //     println!("({},{}) - distance {}", location.0.x, location.0.y, location.1.distance);
-    // }
 
-    let answer1 = dijkstra_explored_map
-        .get(&end_spot)
-        .expect("No dest found")
-        .distance;
-    let answer2 = 0;
+    let answer1 = dijkstra_explored_map.get(&end_spot).expect("No dest found");
+
+    let mut backwards_explored_map = HashMap::from([(end_spot, 0)]);
+    newly_added = backwards_explored_map.clone();
+    let mut answer2 = 0;
+
+    // while dijkstra_explored_map.len() < raw_map.len() * raw_map[0].len() {
+    while answer2 == 0 {
+        let mut newly_newly_added = HashMap::new();
+        newly_added.iter().for_each(|(source, current_distance)| {
+            let current_height = raw_map[source.y as usize][source.x as usize];
+            let current_height = if current_height == 'E' {
+                'z'
+            } else {
+                current_height
+            };
+            [
+                Direction::Up,
+                Direction::Right,
+                Direction::Down,
+                Direction::Left,
+            ]
+            .iter()
+            .for_each(|direction| {
+                let candidate_coords = source.sum(&Coord2::movement(direction));
+                if candidate_coords.y >= 0
+                    && candidate_coords.y < raw_map.len() as i64
+                    && candidate_coords.x >= 0
+                    && candidate_coords.x < raw_map[candidate_coords.y as usize].len() as i64
+                    && !backwards_explored_map.contains_key(&candidate_coords)
+                {
+                    // New location we can consider moving to!
+                    let candidate_height =
+                        raw_map[candidate_coords.y as usize][candidate_coords.x as usize];
+                    let candidate_height = if candidate_height == 'S' {
+                        'a'
+                    } else {
+                        candidate_height
+                    };
+
+                    // Rely on ASCII ordering!
+                    if candidate_height as usize >= current_height as usize - 1 {
+                        if candidate_height == 'a' && answer2 == 0 {
+                            // Found a route, save the answer
+                            answer2 = current_distance + 1;
+                        }
+
+                        backwards_explored_map.insert(candidate_coords, current_distance + 1);
+                        newly_newly_added.insert(candidate_coords, current_distance + 1);
+                    }
+                }
+            })
+        });
+
+        // Replace the newly added list
+        newly_added = newly_newly_added;
+    }
+
     (format!("{}", answer1), format!("{}", answer2))
 }
 
@@ -117,27 +135,9 @@ fn find_char_coords(raw_map: &[Vec<char>], target: &char) -> Coord2 {
     }
 }
 
-#[derive(Clone)]
-struct ExploredSpace {
-    // location: Coord2,
-    distance: usize,
-    _direction_reached: Option<Direction>,
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    // #[test]
-    // fn check_day12_part1_case1() {
-    //     assert_eq!(day12("").0, "0".to_string())
-    // }
-
-    // #[test]
-    // fn check_day12_part2_case1() {
-    //     assert_eq!(day12("").1, "0".to_string())
-    // }
-
     #[test]
     fn check_day12_both_case1() {
         assert_eq!(
@@ -148,7 +148,7 @@ accszExk
 acctuvwj
 abdefghi"
             ),
-            ("31".to_string(), "0".to_string())
+            ("31".to_string(), "29".to_string())
         )
     }
 }
