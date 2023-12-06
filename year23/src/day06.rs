b990832b0bf570e55dd4abbc481392aad398bdc6
Simon Castle <simon.c.castle@gmail.com>
2023-12-06 08:15:22 +0000

    Year 23 Day 06

--- year23/src/day06.rs
+++ year23/src/day06.rs
@@ -1,10 +1,82 @@
 // Potential improvements:
 //
 
-pub fn day06(_input_lines: &[Vec<String>]) -> (String, String) {
-    let answer1 = 0;
-    let answer2 = 0;
+use roots::Roots;
+
+pub fn day06(input_lines: &[Vec<String>]) -> (String, String) {
+    let times = input_lines[0][0]
+        .strip_prefix("Time:")
+        .expect("bad prefix time")
+        .split_whitespace()
+        .map(|time| time.parse::<u32>().expect("couldn't read number"));
+    let distances = input_lines[0][1]
+        .strip_prefix("Distance:")
+        .expect("bad prefix distance")
+        .split_whitespace()
+        .map(|time| time.parse::<u32>().expect("Couldn't read number"));
+
+    let answer1 = times
+        .zip(distances)
+        .map(size_of_quadratic_peak)
+        .product::<u64>();
+
+    let time = input_lines[0][0]
+        .strip_prefix("Time:")
+        .expect("Bad prefix time")
+        .replace(' ', "")
+        .parse::<f64>()
+        .expect("Time not a f64");
+    let distance_string = input_lines[0][1]
+        .strip_prefix("Distance:")
+        .expect("Bad prefix distance")
+        .replace(' ', "");
+    let mut decimal_places = 0;
+    let mut factor = 1f64;
+
+    while distance_string[0..distance_string.len() - decimal_places]
+        .parse::<f64>()
+        .is_err()
+    {
+        decimal_places += 1;
+        factor /= 10f64;
+    }
+
+    let remainder = ("0.".to_string() + &distance_string[distance_string.len() - decimal_places..])
+        .parse::<f64>()
+        .expect("The remainder doesn't work!");
+    let distance = distance_string[0..distance_string.len() - decimal_places]
+        .parse::<f64>()
+        .unwrap()
+        + remainder;
+
+    let time = time * factor;
+    let answer2 = size_of_quadratic_peak_with_factor(factor, time, distance);
+
     (format!("{}", answer1), format!("{}", answer2))
+}
+
+fn size_of_quadratic_peak((t, d): (u32, u32)) -> u64 {
+    // The question equates to finding the integer solutions for the quadratic inequality
+    // x^2 - x*t + d > 0
+
+    size_of_quadratic_peak_with_factor(
+        1f64,
+        -1f64 * TryInto::<f64>::try_into(t).unwrap(),
+        TryInto::<f64>::try_into(d).unwrap(),
+    )
+}
+
+fn size_of_quadratic_peak_with_factor(f: f64, t: f64, d: f64) -> u64 {
+    // The question equates to finding the integer solutions for the quadratic inequality
+    // x^2 - x*t + d > 0
+
+    let roots = roots::find_roots_quadratic(f, -1f64 * t, d);
+    if let Roots::Two([lower, upper]) = roots {
+        let remove_exact_match = if upper.floor() == upper { 1 } else { 0 };
+        (upper.floor() - lower.floor()) as u64 - remove_exact_match
+    } else {
+        panic!("Not got two answers!");
+    }
 }
 
 #[cfg(test)]
@@ -15,9 +87,10 @@ mod tests {
     #[test]
     fn check_day06_case01() {
         full_test(
-            "",  // INPUT STRING
-            "0", // PART 1 RESULT
-            "0", // PART 2 RESULT
+            "Time:      7  15   30
+Distance:  9  40  200", // INPUT STRING
+            "288",   // PART 1 RESULT
+            "71503", // PART 2 RESULT
         )
     }
 
