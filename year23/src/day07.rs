4565a53e431adbc74e4655ed032e707d4d0f3352
Simon Castle <simoncastle@microsoft.com>
2023-12-07 10:41:56 +0000

    Year 23 Day 07 complete

--- year23/src/day07.rs
+++ year23/src/day07.rs
@@ -6,76 +6,60 @@ use std::collections::HashMap;
 use itertools::Itertools;
 
 pub fn day07(input_lines: &[Vec<String>]) -> (String, String) {
-    assert!(HandRank::FiveOfAKind > HandRank::FourOfAKind);
-    assert!(HandRank::ThreeOfAKind > HandRank::TwoPair);
+    let answer1 = input_lines[0]
+        .iter()
+        .map(|line| Hand::from(line, false))
+        .sorted()
+        .enumerate()
+        .map(|(pos, hand)| (pos + 1) * hand.bid)
+        .sum::<usize>();
 
-    let hand1=  Hand::from(&input_lines[0][0]);
-    let hand2 = Hand::from(&input_lines[0][1]);
-    let hand3 = Hand::from(&input_lines[0][2]);
-    let hand4 = Hand::from(&input_lines[0][3]);
-    let hand5 = Hand::from(&input_lines[0][4]);
-    println!("{:?}", hand1);
-    println!("{:?}", hand2);
-    println!("{:?}", hand3);
-    println!("{:?}", hand4);
-    println!("{:?}", hand5);
-
-    assert!(hand1 < hand4);
-    assert!(hand4 < hand3);
-    assert!(hand3 < hand2);
-    assert!(hand2 < hand5);
-
-    let answer1 = input_lines[0].iter().map(Hand::from).sorted().enumerate().map(|(pos, hand)| {
-        println!("{:?}", hand);
-        (pos + 1) * hand.bid}).sum::<usize>();
-
-    let answer2 = 0;
+    let answer2 = input_lines[0]
+        .iter()
+        .map(|line| Hand::from(line, true))
+        .sorted()
+        .enumerate()
+        .map(|(pos, hand)| (pos + 1) * hand.bid)
+        .sum::<usize>();
     (format!("{}", answer1), format!("{}", answer2))
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Debug)]
+/// Hand, with fields ordered to ensure the derived Ord and PartialOrd compare the rank first.
+/// The bid shouldn't technically distinguish hands but we don't expect to ever have an equal hand before that.
+/// (For even cleaner code, that wouldn't even be part of the)
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
 struct Hand {
+    rank: HandRank,
     cards: Vec<u32>,
     bid: usize,
-    rank: HandRank
 }
 
-impl From<&String> for Hand {
-    fn from(line: &String) -> Self {
-        let (hand, bid) = line.split_once(" ").expect("Bad entry line");
+impl Hand {
+    fn from(line: &str, with_jokers: bool) -> Self {
+        let (hand, bid) = line.split_once(' ').expect("Bad entry line");
         assert_eq!(hand.len(), 5);
-        let cards = hand.chars().map(|c| {
-            match c {
+        let cards = hand
+            .chars()
+            .map(|c| match c {
                 'A' => 14,
                 'K' => 13,
                 'Q' => 12,
-                'J' => 11,
+                'J' => {
+                    if with_jokers {
+                        1
+                    } else {
+                        11
+                    }
+                }
                 'T' => 10,
                 _ => c.to_digit(10).expect("not a digit"),
-            }
-        }).collect::<Vec<u32>>();
+            })
+            .collect::<Vec<u32>>();
         let rank = HandRank::rank(&cards);
         Self {
             cards,
             bid: bid.parse::<usize>().expect("Couldn't parse bid"),
             rank,
-        }
-    }
-}
-
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        let rank_comp = self.rank.cmp(&other.rank);
-        if rank_comp.is_eq() {
-            for i in 0..5 {
-                if self.cards[i].cmp(&other.cards[i]).is_ne() {
-                    return self.cards[i].cmp(&other.cards[i])
-                }
-            }
-            std::cmp::Ordering::Equal
-        } else {
-            println!("{:?} is {:?} than {:?}", self.rank, rank_comp, other.rank);
-            rank_comp
         }
     }
 }
@@ -95,38 +79,54 @@ enum HandRank {
 
 impl HandRank {
     fn rank(cards: &[u32]) -> Self {
-        println!("Cards: {:?}", cards);
+        // IMPROVEMENT: make these into statics or OneCell'd or similar
+        let five_of_a_kind = Vec::from([5]);
+        let four_of_a_kind = Vec::from([1, 4]);
+        let full_house = Vec::from([2, 3]);
+        let three_of_a_kind = Vec::from([1, 1, 3]);
+        let two_pair = Vec::from([1, 2, 2]);
+        let pair = Vec::from([1, 1, 1, 2]);
+        let high_card = Vec::from([1, 1, 1, 1, 1]);
+
         let mut card_count = HashMap::new();
         for card in cards {
-            println!("current card_count {:?}", card_count);
-            println!("current card {card}");
-            card_count.entry(card).and_modify(|count| *count += 1).or_insert(1);
+            card_count
+                .entry(card)
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
         }
-        println!("{:?}", card_count.values());
         assert_eq!(card_count.values().sum::<i32>(), 5);
-        let five_of_a_kind = Vec::from([&5]);
-        let four_of_a_kind = Vec::from([&1, &4]);
-        let full_house = Vec::from([&2, &3]);
-        let three_of_a_kind = Vec::from([&1, &1, &3]);
-        let two_pair = Vec::from([&1, &2, &2]);
-        let pair = Vec::from([&1, &1, &1, &2]);
-        let high_card = Vec::from([&1, &1, &1, &1, &1]);
-        let foo = card_count.values().sorted().collect::<Vec<&i32>>();
-        println!("{:?}", foo);
 
-        if foo == five_of_a_kind {
+        // Get the number of jokers and remove its entry from the list.
+        let joker_count = card_count.remove(&1).unwrap_or(0);
+
+        // Collect the hand rank by ordering how many of different values we have; we no longer care what
+        // the values are.
+        let mut hand_collection = card_count.values().sorted().cloned().collect::<Vec<i32>>();
+
+        if hand_collection.is_empty() {
+            // Special case for five jokers
+            hand_collection = five_of_a_kind.clone();
+        } else {
+            // Add any jokers to our single largest set, which is always the last one.
+            let last_index = hand_collection.len() - 1;
+            hand_collection[last_index] += joker_count;
+        }
+
+        // Get the hand rank
+        if hand_collection == five_of_a_kind {
             HandRank::FiveOfAKind
-        } else if foo == four_of_a_kind {
+        } else if hand_collection == four_of_a_kind {
             HandRank::FourOfAKind
-        } else if foo == full_house {
+        } else if hand_collection == full_house {
             HandRank::FullHouse
-        } else if foo == three_of_a_kind {
+        } else if hand_collection == three_of_a_kind {
             HandRank::ThreeOfAKind
-        } else if foo == two_pair {
+        } else if hand_collection == two_pair {
             HandRank::TwoPair
-        } else if foo == pair {
+        } else if hand_collection == pair {
             HandRank::Pair
-        } else if foo == high_card {
+        } else if hand_collection == high_card {
             HandRank::HighCard
         } else {
             panic!()
@@ -156,9 +156,9 @@ mod tests {
 T55J5 684
 KK677 28
 KTJJT 220
-QQQJA 483",  // INPUT STRING
+QQQJA 483", // INPUT STRING
             "6440", // PART 1 RESULT
-            "0", // PART 2 RESULT
+            "5905", // PART 2 RESULT
         )
     }
 
