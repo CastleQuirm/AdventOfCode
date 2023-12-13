89fdd96ef2e1183b5935a6ff8b1cbb96f663b5ec
Simon Castle <simoncastle@microsoft.com>
2023-12-13 10:44:50 +0000

    Year 23 Day 13 Part 2

--- year23/src/day13.rs
+++ year23/src/day13.rs
@@ -4,25 +4,36 @@
 use std::collections::VecDeque;
 
 pub fn day13(input_lines: &[Vec<String>]) -> (String, String) {
-    let answer1 = input_lines.iter().map(|map| find_reflection_value(map)).sum::<usize>();
-    let answer2 = 0;
+    let answer1 = input_lines
+        .iter()
+        .map(|map| find_reflection_value(map, &find_mirror_line))
+        .sum::<usize>();
+    // let answer2 = 0;
+    let answer2 = input_lines
+        .iter()
+        .map(|map| find_reflection_value(map, &find_near_mirror_line))
+        .sum::<usize>();
     (format!("{}", answer1), format!("{}", answer2))
 }
 
-fn find_reflection_value(map: &[String]) -> usize {
+fn find_reflection_value(map: &[String], comparison_function: &dyn Fn(&[String]) -> Option<usize>) -> usize {
     // While we've got ready-formed rows, search for a horizontal line of reflection.
-    if let Some(row_result) = find_mirror_line(map) {
-        return row_result * 100
+    if let Some(row_result) = comparison_function(map) {
+        return row_result * 100;
     }
 
     // Convert the rows into columns
     let mut columns = Vec::new();
     for i in 0..map[0].len() {
-        columns.push(map.iter().map(|row| row.chars().nth(i).unwrap()).collect::<String>());
+        columns.push(
+            map.iter()
+                .map(|row| row.chars().nth(i).unwrap())
+                .collect::<String>(),
+        );
     }
 
     // Return the mirror result, which we must find.
-    find_mirror_line(&columns).expect("Didn't find a mirror line in rows or columns")
+    comparison_function(&columns).expect("Didn't find a mirror line in rows or columns")
 }
 
 fn find_mirror_line(lines: &[String]) -> Option<usize> {
@@ -31,7 +42,31 @@ fn find_mirror_line(lines: &[String]) -> Option<usize> {
     while !rows_to_go.is_empty() {
         let row_count = rows_to_go.len().min(rows_passed.len());
         if (0..row_count).all(|i| rows_to_go[i] == rows_passed[i]) {
-            return Some(rows_passed.len())
+            return Some(rows_passed.len());
+        }
+        rows_passed.push_front(rows_to_go.pop_front().unwrap())
+    }
+    None
+}
+
+fn count_differences(line: &str, other: &str) -> usize {
+    assert_eq!(line.len(), other.len());
+    (0..line.len())
+        .filter(|&i| line.chars().nth(i) != other.chars().nth(i))
+        .count()
+}
+
+fn find_near_mirror_line(lines: &[String]) -> Option<usize> {
+    let mut rows_to_go = lines.iter().cloned().collect::<VecDeque<String>>();
+    let mut rows_passed = VecDeque::from([rows_to_go.pop_front().unwrap()]);
+    while !rows_to_go.is_empty() {
+        let row_count = rows_to_go.len().min(rows_passed.len());
+        if (0..row_count)
+            .map(|i| count_differences(&rows_to_go[i], &rows_passed[i]))
+            .sum::<usize>()
+            == 1
+        {
+            return Some(rows_passed.len());
         }
         rows_passed.push_front(rows_to_go.pop_front().unwrap())
     }
@@ -60,9 +95,9 @@ mod tests {
 #####.##.
 #####.##.
 ..##..###
-#....#..#",  // INPUT STRING
+#....#..#", // INPUT STRING
             "405", // PART 1 RESULT
-            "0", // PART 2 RESULT
+            "400", // PART 2 RESULT
         )
     }
 
