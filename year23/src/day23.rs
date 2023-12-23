0e30148c9c6fb7e57aafa89f41cd5d49b312e578
Simon Castle <simon.c.castle@gmail.com>
2023-12-23 11:30:25 +0000

    Year 23 Day 23

--- year23/src/day23.rs
+++ year23/src/day23.rs
@@ -1,10 +1,212 @@
 // Potential improvements:
 //
 
-pub fn day23(_input_lines: &[Vec<String>]) -> (String, String) {
-    let answer1 = 0;
-    let answer2 = 0;
+use std::collections::{HashMap, HashSet};
+
+use itertools::Itertools;
+
+use crate::{
+    coord::Coord2,
+    directions::{
+        CompassDirection::{self, East, North, South, West},
+        Rotation,
+    },
+    grid::Grid,
+};
+
+pub fn day23(input_lines: &[Vec<String>]) -> (String, String) {
+    // Get the maze. It comes with a border!
+    let maze = Grid::<MazeSpace>::from_input(&input_lines[0]);
+
+    // Find the start and end. The example and my input both have the start at (1, 0) and the end
+    // at (width - 1, height), but the problem only states they're in the top and bottom row so
+    // we'll be polite and find them from that info.
+    let find_space_in_row = |y: usize| Coord2 {
+        x: maze.grid[y]
+            .iter()
+            .find_position(|s| **s == MazeSpace::Space)
+            .and_then(|(pos, _)| TryInto::<i64>::try_into(pos).ok())
+            .expect("Error finding entrance"),
+        y: TryInto::<i64>::try_into(y).expect("Bad y index"),
+    };
+    let start = find_space_in_row(0);
+    let end = find_space_in_row(maze.grid.len() - 1);
+
+    // Explore the maze, building up a series of paths.
+    let paths = explore(&maze, &start, &end);
+
+    // Explore the set of paths, looking for the longest continuous route without repeats.
+    let answer1 = longest_route(&paths, &start, &end, true);
+    let answer2 = longest_route(&paths, &start, &end, false);
     (format!("{}", answer1), format!("{}", answer2))
+}
+
+fn explore(maze: &Grid<MazeSpace>, start: &Coord2, end: &Coord2) -> HashMap<Coord2, PathsFrom> {
+    let mut established_paths: HashMap<Coord2, PathsFrom> = HashMap::new();
+    let mut to_explore: Vec<(Coord2, CompassDirection)> = Vec::from([(*start, South)]);
+    while let Some((rally_point, start_dir)) = to_explore.pop() {
+        let mut current_loc = rally_point;
+        let mut possible_next_dirs = Vec::from([start_dir]);
+        let mut steps_walked = 0;
+        let mut current_dir = possible_next_dirs[0];
+        let mut difficult_terrain = false;
+
+        while possible_next_dirs.len() == 1 {
+            current_dir = possible_next_dirs[0];
+            current_loc = current_loc.compass_sum(&current_dir);
+            steps_walked += 1;
+            if current_loc == *end || current_loc == *start {
+                // If we've reached the exit, we should stop this particular exploration.
+                break;
+            }
+            match maze.get(&current_loc) {
+                MazeSpace::Space => {
+                    // Check where we can go next (without reversing)
+                    possible_next_dirs = [Rotation::Straight, Rotation::Left, Rotation::Right]
+                        .into_iter()
+                        .filter_map(|rot| {
+                            let new_dir = current_dir.rotate(&rot);
+                            if maze.get(&current_loc.compass_sum(&new_dir)) != MazeSpace::Wall {
+                                Some(new_dir)
+                            } else {
+                                None
+                            }
+                        })
+                        .collect_vec();
+                }
+                MazeSpace::OneWay { allowed_dir } => {
+                    // This is always just a straigh-on path so we don't need to change
+                    // possible_next_dirs
+                    assert!(allowed_dir == current_dir || allowed_dir == current_dir.opposite());
+                    // If this isn't the direction of the arrow, then this trail is difficult.
+                    if allowed_dir == current_dir.opposite() {
+                        difficult_terrain = true;
+                    }
+                }
+                MazeSpace::Wall => unreachable!("Shouldn't be walking into walls!"),
+            }
+        }
+
+        if !possible_next_dirs.is_empty() {
+            // We've established a path.
+            let known_trail = established_paths
+                .entry(rally_point)
+                .or_default()
+                .routes
+                .insert(
+                    start_dir,
+                    Trail {
+                        destination: current_loc,
+                        distance: steps_walked,
+                        incoming_dir: current_dir,
+                        difficult_terrain,
+                    },
+                );
+            assert!(known_trail.is_none());
+
+            // If there was more than one possible direction left and we haven't already explored this site, this is a junction to explore on from.
+            if possible_next_dirs.len() > 1 && !established_paths.contains_key(&current_loc) {
+                for dir in possible_next_dirs {
+                    to_explore.push((current_loc, dir));
+                }
+                to_explore.push((current_loc, current_dir.opposite()))
+            }
+        }
+    }
+    established_paths
+}
+
+fn longest_route(
+    paths: &HashMap<Coord2, PathsFrom>,
+    start: &Coord2,
+    end: &Coord2,
+    block_difficult_terrain: bool,
+) -> usize {
+    let mut longest_walk = 0;
+
+    let mut ongoing_walks: Vec<Walk> = Vec::from([Walk {
+        current_loc: *start,
+        visited_junctions: HashSet::from([*start]),
+        distance: 0,
+        latest_dir: South,
+    }]);
+    while let Some(walk) = ongoing_walks.pop() {
+        paths
+            .get(&walk.current_loc)
+            .expect("Unknown junction!")
+            .routes
+            .iter()
+            .for_each(|(outgoing_dir, trail)| {
+                if *outgoing_dir == walk.latest_dir.opposite() {
+                    // Don't want to just turn around
+                    return;
+                }
+                if block_difficult_terrain && trail.difficult_terrain {
+                    // This trail has difficult terrain, which isn't allowed at the moment
+                    return;
+                }
+                let next_junction = trail.destination;
+                let new_distance = walk.distance + trail.distance;
+                if next_junction == *end {
+                    // Path reached the end, check the overall walk distance
+                    longest_walk = longest_walk.max(new_distance);
+                } else if !walk.visited_junctions.contains(&next_junction) {
+                    let mut visited_junctions = walk.visited_junctions.clone();
+                    visited_junctions.insert(next_junction);
+                    ongoing_walks.push(Walk {
+                        current_loc: next_junction,
+                        visited_junctions,
+                        distance: new_distance,
+                        latest_dir: trail.incoming_dir,
+                    })
+                }
+            });
+    }
+
+    longest_walk
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Default)]
+struct PathsFrom {
+    routes: HashMap<CompassDirection, Trail>,
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+struct Trail {
+    destination: Coord2,
+    distance: usize,
+    incoming_dir: CompassDirection,
+    difficult_terrain: bool,
+}
+
+#[derive(Clone, Eq, PartialEq, Debug)]
+struct Walk {
+    current_loc: Coord2,
+    visited_junctions: HashSet<Coord2>,
+    distance: usize,
+    latest_dir: CompassDirection,
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+enum MazeSpace {
+    Space,
+    OneWay { allowed_dir: CompassDirection },
+    Wall,
+}
+
+impl From<char> for MazeSpace {
+    fn from(value: char) -> Self {
+        match value {
+            '.' => MazeSpace::Space,
+            '>' => MazeSpace::OneWay { allowed_dir: East },
+            '<' => MazeSpace::OneWay { allowed_dir: West },
+            'v' => MazeSpace::OneWay { allowed_dir: South },
+            '^' => MazeSpace::OneWay { allowed_dir: North },
+            // '.' | '<' | '>' | '^' | 'v' => MazeSpace::Space,
+            '#' => MazeSpace::Wall,
+            _ => panic!(),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -15,9 +217,31 @@ mod tests {
     #[test]
     fn check_day23_case01() {
         full_test(
-            "",  // INPUT STRING
-            "0", // PART 1 RESULT
-            "0", // PART 2 RESULT
+            "#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#", // INPUT STRING
+            "94",  // PART 1 RESULT
+            "154", // PART 2 RESULT
         )
     }
 
--- year23/src/directions.rs
+++ year23/src/directions.rs
@@ -35,10 +35,10 @@ impl Direction {
             (Direction::Up, Rotation::Right) => Direction::Right,
             (Direction::Down, Rotation::Left) => Direction::Right,
             (Direction::Down, Rotation::Right) => Direction::Left,
-            (Direction::Left, Rotation::_Reverse) => Direction::Right,
-            (Direction::Right, Rotation::_Reverse) => Direction::Left,
-            (Direction::Up, Rotation::_Reverse) => Direction::Down,
-            (Direction::Down, Rotation::_Reverse) => Direction::Up,
+            (Direction::Left, Rotation::Reverse) => Direction::Right,
+            (Direction::Right, Rotation::Reverse) => Direction::Left,
+            (Direction::Up, Rotation::Reverse) => Direction::Down,
+            (Direction::Down, Rotation::Reverse) => Direction::Up,
         }
     }
 
@@ -49,20 +49,20 @@ impl Direction {
     pub fn count_rotation(&self, turn_to: &Self) -> Rotation {
         match (self, turn_to) {
             (Direction::Left, Direction::Left) => Rotation::Straight,
-            (Direction::Left, Direction::Right) => Rotation::_Reverse,
+            (Direction::Left, Direction::Right) => Rotation::Reverse,
             (Direction::Left, Direction::Up) => Rotation::Right,
             (Direction::Left, Direction::Down) => Rotation::Left,
-            (Direction::Right, Direction::Left) => Rotation::_Reverse,
+            (Direction::Right, Direction::Left) => Rotation::Reverse,
             (Direction::Right, Direction::Right) => Rotation::Straight,
             (Direction::Right, Direction::Up) => Rotation::Left,
             (Direction::Right, Direction::Down) => Rotation::Right,
             (Direction::Up, Direction::Left) => Rotation::Left,
             (Direction::Up, Direction::Right) => Rotation::Right,
             (Direction::Up, Direction::Up) => Rotation::Straight,
-            (Direction::Up, Direction::Down) => Rotation::_Reverse,
+            (Direction::Up, Direction::Down) => Rotation::Reverse,
             (Direction::Down, Direction::Left) => Rotation::Right,
             (Direction::Down, Direction::Right) => Rotation::Left,
-            (Direction::Down, Direction::Up) => Rotation::_Reverse,
+            (Direction::Down, Direction::Up) => Rotation::Reverse,
             (Direction::Down, Direction::Down) => Rotation::Straight,
         }
     }
@@ -77,9 +77,9 @@ pub enum CompassDirection {
 }
 
 impl CompassDirection {
-    // pub fn opposite(&self) -> Self {
-    //     self.rotate(&Rotation::Reverse)
-    // }
+    pub fn opposite(&self) -> Self {
+        self.rotate(&Rotation::Reverse)
+    }
 
     pub fn rotate(&self, rotation: &Rotation) -> Self {
         // Guess I could implement a 'degrees' system to make this a bit less text heavy but...eh
@@ -87,16 +87,16 @@ impl CompassDirection {
             (direction, Rotation::Straight) => *direction,
             (Self::North, Rotation::Left) => Self::West,
             (Self::North, Rotation::Right) => Self::East,
-            (Self::North, Rotation::_Reverse) => Self::South,
+            (Self::North, Rotation::Reverse) => Self::South,
             (Self::East, Rotation::Left) => Self::North,
             (Self::East, Rotation::Right) => Self::South,
-            (Self::East, Rotation::_Reverse) => Self::West,
+            (Self::East, Rotation::Reverse) => Self::West,
             (Self::South, Rotation::Left) => Self::East,
             (Self::South, Rotation::Right) => Self::West,
-            (Self::South, Rotation::_Reverse) => Self::North,
+            (Self::South, Rotation::Reverse) => Self::North,
             (Self::West, Rotation::Left) => Self::South,
             (Self::West, Rotation::Right) => Self::North,
-            (Self::West, Rotation::_Reverse) => Self::East,
+            (Self::West, Rotation::Reverse) => Self::East,
         }
     }
 }
@@ -106,7 +106,7 @@ pub enum Rotation {
     Left,
     Right,
     Straight,
-    _Reverse,
+    Reverse,
 }
 
 impl FromStr for Rotation {
