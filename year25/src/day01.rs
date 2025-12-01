589a46ab5859d177b0304fc952ac1239f947d744
Simon Castle <simon.c.castle@gmail.com>
2025-12-01 08:14:09 +0000

    Year 25 Day 01 - here we go again

--- year25/src/day01.rs
+++ year25/src/day01.rs
@@ -1,9 +1,32 @@
 // Potential improvements:
 //
 
-pub fn day01(_input_lines: &[Vec<String>]) -> (String, String) {
-    let answer1 = 0;
-    let answer2 = 0;
+pub fn day01(input_lines: &[Vec<String>]) -> (String, String) {
+    let mut answer1 = 0;
+    let mut answer2 = 0;
+    let mut dial = 50;
+    input_lines[0].iter().for_each(|line| {
+        let direction_mult = match line.chars().next().expect("empty line?") {
+            'L' => -1,
+            'R' => 1,
+            _ => panic!("Unknown direction"),
+        };
+        let value = line[1..].parse::<i32>().expect("Couldn't parse number");
+        answer2 += value / 100;
+        // horrible hacks
+        if dial == 0 && direction_mult == -1 {
+            answer2 -= 1;
+        }
+        dial += value.rem_euclid(100) * direction_mult;
+        if dial.rem_euclid(100) != dial || dial == 0 {
+            answer2 += 1;
+        }
+        dial = dial.rem_euclid(100);
+        if dial == 0 {
+            answer1 += 1;
+        }
+    });
+
     (format!("{}", answer1), format!("{}", answer2))
 }
 
@@ -15,14 +38,26 @@ mod tests {
     #[test]
     fn check_day01_case01() {
         full_test(
-"", // INPUT STRING
-"0", // PART 1 RESULT
-"0" // PART 2 RESULT
+            "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82", // INPUT STRING
+            "3", // PART 1 RESULT
+            "6", // PART 2 RESULT
         )
     }
 
     fn full_test(input_text: &str, part1_result: &str, part2_result: &str) {
         let input_lines = load_input(input_text);
-        assert_eq!(day01(&input_lines), (part1_result.to_string(), part2_result.to_string()));
+        assert_eq!(
+            day01(&input_lines),
+            (part1_result.to_string(), part2_result.to_string())
+        );
     }
 }
