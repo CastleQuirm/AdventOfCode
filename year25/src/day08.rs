99a119c3ec7894d31606e8388e5ceb45cce7843d
Simon Castle <simon.c.castle@gmail.com>
2025-12-08 15:07:09 +0000

    Year 25 Day 08 Part 2

--- year25/src/day08.rs
+++ year25/src/day08.rs
@@ -1,64 +1,68 @@
 // Potential improvements:
 //
 
-use std::str::FromStr;
+use std::{collections::HashSet, str::FromStr};
 
 use grid::coord::Coord3;
 use itertools::Itertools;
 
 pub fn day08(input_lines: &[Vec<String>]) -> (String, String) {
-    let cables = input_lines[0]
+    let coords = input_lines[0]
         .iter()
         .map(|line| Coord3::from_str(line).expect("Bad coord"))
+        .collect::<Vec<Coord3>>();
+    let cables = coords
+        .iter()
         .enumerate()
         .combinations(2)
         .map(|combi| Cabling {
-            square_dist: combi[0].1.eucl_dist_squared(&combi[1].1),
+            square_dist: combi[0].1.eucl_dist_squared(combi[1].1),
             first: combi[0].0,
             second: combi[1].0,
         })
         .sorted()
         .collect::<Vec<Cabling>>();
 
-    // This is an array that gives the lowest indexed junction box that that index of box connects to.
-    let mut primaries = input_lines[0]
+    // Create a set of routers to track state.
+    let mut routers = input_lines[0]
         .iter()
         .enumerate()
-        .map(|(i, _)| i)
+        .map(|(index, _)| Router {
+            index,
+            conn_type: ConnectionType::Unconnected,
+        })
         .collect::<Vec<_>>();
 
-    let used_conns = if primaries.len() == 20 {
+    let part1_conns = if routers.len() == 20 {
         10 // test code
     } else {
         1000 // real input
     };
 
-    // This updates the set of primaries for each of the used connections.
-    cables[0..used_conns].iter().for_each(|cable| {
-        let lowest_source = primaries[cable.first].min(primaries[cable.second]);
-        let replaced_source = primaries[cable.first].max(primaries[cable.second]);
-        primaries = primaries
-            .iter()
-            .map(|source| {
-                if *source == replaced_source {
-                    lowest_source
-                } else {
-                    *source
-                }
-            })
-            .collect::<Vec<usize>>();
+    // Perform all the connections for part 1.
+    cables[0..part1_conns].iter().for_each(|cable| {
+        cable.apply_cable(&mut routers);
     });
 
-    // Now need to count up each possible index for how many use it as a primary, and mutliply the three highest values.
-    let mut dependents = vec![0; primaries.len()];
-    primaries.iter().for_each(|source| {
-        dependents[*source] += 1;
-    });
-    dependents.sort();
-    dependents.reverse();
+    // Get the set of network sizes.
+    let mut network_sizes = routers
+        .iter()
+        .filter_map(|r| r.powered_count())
+        .collect::<Vec<usize>>();
+    network_sizes.sort();
+    network_sizes.reverse();
+    let answer1 = network_sizes[0..3].iter().product::<usize>();
 
-    let answer1 = dependents[0] * dependents[1] * dependents[2];
-    let answer2 = 0;
+    // And part2...
+    let mut further_cables = cables[part1_conns..].iter();
+    let mut cand_cable = cables[part1_conns];
+    while routers[0].powered_count() != Some(routers.len()) {
+        let next_cable = further_cables.next().expect("Must still have a cable");
+        next_cable.apply_cable(&mut routers);
+        cand_cable = *next_cable;
+    }
+
+    let answer2 = coords[cand_cable.first].x * coords[cand_cable.second].x;
     (format!("{}", answer1), format!("{}", answer2))
 }
 
@@ -67,6 +71,110 @@ struct Cabling {
     square_dist: i64,
     first: usize,
     second: usize,
+}
+
+impl Cabling {
+    fn apply_cable(&self, routers: &mut [Router]) {
+        routers[self.first]
+            .clone()
+            .connect(&routers[self.second].clone(), routers);
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Router {
+    index: usize,
+    conn_type: ConnectionType,
+}
+
+impl Router {
+    fn connect(&self, other: &Router, array: &mut [Router]) {
+        // Find the lowest source and label the objects to act on them.
+        let (new_source, secondary) = match self.source().cmp(&other.source()) {
+            std::cmp::Ordering::Less => (self.source(), other),
+            std::cmp::Ordering::Equal => return, // Nothing needs to happen!
+            std::cmp::Ordering::Greater => (other.source(), self),
+        };
+
+        // Work out what the higher's contents are and redirect them first.
+        let merged_routers = match &secondary.conn_type {
+            ConnectionType::Unconnected => {
+                array[secondary.index].conn_type = ConnectionType::Powered { source: new_source };
+                HashSet::from([secondary.index])
+            }
+            ConnectionType::Source { .. } => secondary.source_becomes_powered(new_source, array),
+            ConnectionType::Powered { source } => array[*source]
+                .clone()
+                .source_becomes_powered(new_source, array),
+        };
+
+        // New source needs updating to (a) make sure its a Source and (b) has all the contents of the other
+        let updating_source = array.get_mut(new_source).expect("Bad index");
+        match &updating_source.conn_type {
+            ConnectionType::Unconnected => {
+                updating_source.conn_type = ConnectionType::Source {
+                    powered: merged_routers,
+                }
+            }
+            ConnectionType::Source {
+                powered: already_powered,
+            } => {
+                updating_source.conn_type = ConnectionType::Source {
+                    powered: merged_routers
+                        .union(already_powered)
+                        .cloned()
+                        .collect::<HashSet<_>>(),
+                };
+            }
+            ConnectionType::Powered { .. } => panic!("This shouldn't have happened"),
+        }
+    }
+
+    fn source_becomes_powered(&self, new_source: usize, array: &mut [Router]) -> HashSet<usize> {
+        match &self.conn_type {
+            ConnectionType::Source { powered } => {
+                powered.iter().for_each(|downstream_ix| {
+                    let updated_router = array.get_mut(*downstream_ix).expect("bad router index");
+                    assert!(matches!(
+                        updated_router.conn_type,
+                        ConnectionType::Powered { source: _ }
+                    ));
+                    updated_router.conn_type = ConnectionType::Powered { source: new_source };
+                });
+                array
+                    .get_mut(self.index)
+                    .expect("bad router index")
+                    .conn_type = ConnectionType::Powered { source: new_source };
+                let mut new_contents = powered.clone();
+                new_contents.insert(self.index);
+                new_contents
+            }
+            _ => {
+                panic!("Bad caller")
+            }
+        }
+    }
+
+    fn source(&self) -> usize {
+        match self.conn_type {
+            ConnectionType::Powered { source } => source,
+            _ => self.index,
+        }
+    }
+
+    fn powered_count(&self) -> Option<usize> {
+        match &self.conn_type {
+            ConnectionType::Source { powered } => Some(powered.len() + 1),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+enum ConnectionType {
+    Unconnected,
+    Source { powered: HashSet<usize> },
+    Powered { source: usize },
 }
 
 #[cfg(test)]
@@ -97,8 +205,8 @@ mod tests {
 862,61,35
 984,92,344
 425,690,689", // INPUT STRING
-            "40", // PART 1 RESULT
-            "0",  // PART 2 RESULT
+            "40",    // PART 1 RESULT
+            "25272", // PART 2 RESULT
         )
     }
 
